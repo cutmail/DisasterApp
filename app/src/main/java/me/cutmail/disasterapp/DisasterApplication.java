0a4ece5da7914b18cb6ee23cd1270ee741f8af5d
cutmail <sunday.is.over@gmail.com>
2015-01-18 15:08:37 +0900

    Add Google Analytics

--- app/src/main/java/me/cutmail/disasterapp/DisasterApplication.java
+++ app/src/main/java/me/cutmail/disasterapp/DisasterApplication.java
@@ -2,12 +2,35 @@ package me.cutmail.disasterapp;
 
 import android.app.Application;
 
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.Tracker;
 import com.parse.Parse;
 import com.parse.ParseCrashReporting;
 import com.parse.ParseInstallation;
 import com.parse.ParsePush;
 
+import java.util.HashMap;
+
 public class DisasterApplication extends Application {
+
+    private static final String PROPERTY_ID = "UA-3314949-13";
+
+    public enum TrackerName {
+        APP_TRACKER,
+        GLOBAL_TRACKER
+    }
+
+    HashMap<TrackerName, Tracker> trackers = new HashMap<TrackerName, Tracker>();
+
+    synchronized Tracker getTracker(TrackerName trackerId) {
+        if (!trackers.containsKey(trackerId)) {
+            GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
+            Tracker t = analytics.newTracker(PROPERTY_ID);
+            trackers.put(trackerId, t);
+        }
+
+        return trackers.get(trackerId);
+    }
 
     @Override
     public void onCreate() {
